a1ef69cd8d64f185ca27089c0b667d66c14d6272
Hojayfa Rahman <hojayfarahman@gmail.com>
2025-08-27 17:00:50 +0600

    chore: disable session auth

--- core/settings.py
+++ core/settings.py
@@ -301,7 +301,7 @@ UNFOLD = UNFOLD_CONFIG
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
